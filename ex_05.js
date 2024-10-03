4c90284f35332a9c67d952f83b6ccbeb1c403ba3
QueenMae <maena.brice@epitech.digital>
2024-10-03 16:55:56 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+  let par = document.getElementById("addToCart")
+  par.addEventListener("click", function(){
+    alert("Thanks !")
+  } )
+})
